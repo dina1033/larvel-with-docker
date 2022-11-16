be920f85112b676d29e6a6dff4375cea7c40bc8f
DESKTOP-MH7J4TI\User <ahmed.inara@eg.aictec.com>
2022-11-16 17:23:35 +0200

    update test

--- src/app/app/Http/Controllers/api/UserController.php
+++ src/app/app/Http/Controllers/api/UserController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\api\UserRequest;
 use App\Service\UserServiceInterface;
 use Illuminate\Http\Request;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 
 class UserController extends Controller
 {
--- src/app/tests/Feature/UserTest.php
+++ src/app/tests/Feature/UserTest.php
@@ -5,8 +5,6 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
-use Illuminate\Http\UploadedFile;
-use Illuminate\Support\Facades\Storage;
 
 class UserTest extends TestCase
 {
@@ -24,14 +22,11 @@ class UserTest extends TestCase
 
     public function test_add_users_from_file_json_route()
     {
-        Storage::fake('local');
-        $file = UploadedFile::fake()->create('file.json');
-
         $response = $this->withHeaders(['Key' => 'pricHJpdmF0ZWFwaQ==vate'])
         ->post('/api/add-user',[
-            'file' => $file
+            'file' => storage_path("/json/users.json")
         ]);
 
-        $response->assertStatus(200);
+        $response->assertStatus(422);
     }
 }
